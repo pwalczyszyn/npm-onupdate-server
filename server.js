4672800c3bc43998b9826e7e00eedede64f9f576
Piotr Walczyszyn <piotr.walczyszyn@gmail.com>
2013-10-15 22:40:18 +0200

    added changes couchdb api

--- server.js
+++ server.js
@@ -20,3 +20,5 @@ require('./config/routes')(app);
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
+
+require('./server/monitor').start();
--- server/monitor/index.js
+++ server/monitor/index.js
@@ -1,8 +1,34 @@
 /* jshint node:true */
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+    Changes = require('changes'),
+    changes = new Changes({
+        url: 'http://isaacs.iriscouch.com/registry',
+        timeout: {
+            max: 60000,
+            step: 5000
+        },
+        updateSeq: 726035
+    });
+
+//
+// Dump changes as they come in.
+//
+changes.on('change', function (doc) {
+    console.log('\n________________________________________________________________\n');
+    console.dir(doc);
+});
+
 
 exports.start = function () {
 
+    changes.listen(function (err) {
+        if (err) {
+            console.log('Did not connect to _changes on first attempt');
+            console.dir(err);
+            changes.retry.enabled = false;
+            return process.exit(1);
+        }
 
-
+        console.log('Listening for _changes');
+    });
 }
