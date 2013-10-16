44e576432eb01a0c2e6712f796b3d946d6f558e7
Piotr Walczyszyn <piotr.walczyszyn@gmail.com>
2013-10-16 20:34:56 +0200

    added db openshift mongodb settings

--- config/config.js
+++ config/config.js
@@ -2,5 +2,8 @@
 module.exports = {
     development: {
         db: 'mongodb://localhost/onupdate'
+    },
+    openshift: {
+        db: 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' + process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/' + process.env.OPENSHIFT_APP_NAME
     }
 };
