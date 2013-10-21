a91230206d96777333cb8c5131257cf3013c6dc1
Piotr Walczyszyn <piotr.walczyszyn@gmail.com>
2013-10-21 15:28:37 +0200

    added simple docs

--- config/express.js
+++ config/express.js
@@ -9,11 +9,11 @@ module.exports = function (app) {
     app.set('views', path.join(__dirname, '../server/views'));
     app.set('view engine', 'ejs');
     app.use(express.static(path.join(__dirname, '../client/static')));
-    app.use(express.favicon());
-    app.use(express.logger('dev'));
+//    app.use(express.favicon());
     app.use(express.json()).use(express.urlencoded());
     app.use(express.methodOverride());
     app.use(expressValidator());
+    app.use(express.logger('dev'));
     app.use(app.router);
 
     // development only
